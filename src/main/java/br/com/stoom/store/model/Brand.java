cf3410a0a900b2347dd5563c9433f7d444659dc9
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-05-18 16:06:46 -0300

    Adicionado getters, setters, contructors, equals and hashcode

--- src/main/java/br/com/stoom/store/model/Brand.java
+++ src/main/java/br/com/stoom/store/model/Brand.java
@@ -2,6 +2,7 @@ package br.com.stoom.store.model;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "t_brands")
@@ -25,4 +26,59 @@ public class Brand {
     @OneToMany(mappedBy = "brand", cascade = CascadeType.DETACH)
     private List<Product> products;
 
+    public Brand() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return active == brand.active && Objects.equals(id, brand.id) && Objects.equals(name, brand.name) && Objects.equals(description, brand.description) && Objects.equals(products, brand.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, active, products);
+    }
 }
