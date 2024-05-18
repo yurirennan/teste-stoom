9989b97d24b659c29b44a0b74b04af39d583c4d1
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-05-18 16:08:50 -0300

    Criação da camada de controller que contém os endpoints da entidade Brand

---
+++ src/main/java/br/com/stoom/store/controller/BrandController.java
@@ -0,0 +1,56 @@
+package br.com.stoom.store.controller;
+
+import br.com.stoom.store.business.interfaces.IBrandBO;
+import br.com.stoom.store.business.interfaces.ICategoryBO;
+import br.com.stoom.store.dto.brand.CreateBrandRequestDTO;
+import br.com.stoom.store.dto.brand.ReadBrandResponseDTO;
+import br.com.stoom.store.dto.brand.UpdateBrandStatusDTO;
+import br.com.stoom.store.dto.category.CreateCategoryRequestDTO;
+import br.com.stoom.store.dto.category.ReadCategoryResponseDTO;
+import br.com.stoom.store.dto.category.UpdateCategoryStatusDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandController {
+
+    private final IBrandBO brandBO;
+
+    @Autowired
+    public BrandController(final IBrandBO brandBO) {
+        this.brandBO = brandBO;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> saveBrand(@RequestBody CreateBrandRequestDTO createBrandRequestDTO) {
+        this.brandBO.createBrand(createBrandRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReadBrandResponseDTO> listBrand(@PathVariable("id") final Long brandId) {
+        final ReadBrandResponseDTO readBrandResponseDTO = this.brandBO.listBrand(brandId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(readBrandResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReadBrandResponseDTO>> listAllBrands() {
+        final List<ReadBrandResponseDTO> readBrandResponseDTOList = this.brandBO.listAllBrands();
+
+        return ResponseEntity.status(HttpStatus.OK).body(readBrandResponseDTOList);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> updateBrandStatus(@PathVariable("id") final Long brandId,
+                                                  @RequestBody final UpdateBrandStatusDTO updateBrandStatusDTO) {
+        this.brandBO.updateBrandStatus(brandId, updateBrandStatusDTO);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
