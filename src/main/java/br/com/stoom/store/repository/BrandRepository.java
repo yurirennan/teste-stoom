03b65282f3c7818342c990b0931b6f43f0ac2ffd
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-05-18 18:44:38 -0300

    Adicionado método para buscar uma marca por id e se ela está ativa

--- src/main/java/br/com/stoom/store/repository/BrandRepository.java
+++ src/main/java/br/com/stoom/store/repository/BrandRepository.java
@@ -1,10 +1,13 @@
 package br.com.stoom.store.repository;
 
 import br.com.stoom.store.model.Brand;
+import br.com.stoom.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
@@ -12,5 +15,7 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
     @Modifying
     @Query("UPDATE Brand b SET b.active = ?2 WHERE b.id = ?1")
     void updateBrandStatusById(final Long brandId, final boolean status);
+
+    Optional<Brand> findBrandByIdAndActiveTrue(final Long brandId);
 
 }
