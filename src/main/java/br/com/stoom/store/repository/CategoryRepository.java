646822ecb60412e46b102a8cd439e5ce518e9db4
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-05-18 18:56:36 -0300

    Criado método para buscar uma categoria pelo id que esteja ativa

--- src/main/java/br/com/stoom/store/repository/CategoryRepository.java
+++ src/main/java/br/com/stoom/store/repository/CategoryRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     @Modifying
     @Query("UPDATE Category c SET c.active = ?2 WHERE c.id = ?1")
     void updateCategoryStatusById(final Long categoryId, final boolean status);
+
+    Optional<Category> findCategoryByIdAndActiveTrue(final Long categoryId);
 
 }
